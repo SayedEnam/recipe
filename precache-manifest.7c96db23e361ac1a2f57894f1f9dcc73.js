5685bbc93a632c0420ae148cf9d13af744f3af4c
SayedEnam <syedenamshohag@gmail.com>
2019-09-13 23:03:00 +0600

    Updates

---
+++ precache-manifest.7c96db23e361ac1a2f57894f1f9dcc73.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cb1dfdc79b8e7ea89de19f324bd730b",
+    "url": "/recipe/index.html"
+  },
+  {
+    "revision": "57df63ee565930e62d4c",
+    "url": "/recipe/static/css/main.50d1fdbe.chunk.css"
+  },
+  {
+    "revision": "7a07a07effeac42a7cb8",
+    "url": "/recipe/static/js/2.21fa59c4.chunk.js"
+  },
+  {
+    "revision": "57df63ee565930e62d4c",
+    "url": "/recipe/static/js/main.f89f1930.chunk.js"
+  },
+  {
+    "revision": "7440b4db17ce2cc862c6",
+    "url": "/recipe/static/js/runtime~main.1824b14a.js"
+  }
+]);
